2518104ccd34af5ed9c2db9843f7df7d961ebdfd
Falino <alessandrofalino@gmail.com>
2023-09-11 17:17:34 +0200

    a

--- GenerazioneLog.py
+++ GenerazioneLog.py
@@ -1,6 +1,7 @@
 from transformers import AutoModelForSequenceClassification
 from transformers import AutoTokenizer
 import torch
+from datasets import load_dataset
 
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
@@ -31,3 +32,46 @@ predictions = torch.argmax(logits, dim=1)
 print("Classe predetta:", predictions.item())
 
 #aggiungere il log per intero
+dataset = load_dataset("json", data_files={"train": "C:\\Users\\GAMING EDGE\\Desktop\\LAUREA\\Datasets\\V4\\dataset-involved_services-raw_logs-10000-with_labels.json"})
+
+labels = []
+
+def is_string_in_array(string, string_array):
+    return string in string_array
+
+def trova_labels_unici(dataset):
+    labels = []
+    for val in dataset:
+
+        # Estrai la stringa dal JSON
+        data_string = dataset[val]["label"]
+
+        # Rimuovi le stringhe duplicate mantenendo l'ordine dell'apparizione
+        labels = list(dict.fromkeys(data_string))
+            
+    return labels
+
+temp_list = trova_labels_unici(dataset)
+
+# dictionary that maps integer to its string value 
+label_dict = {}
+
+# list to store integer labels 
+int_labels = []
+
+for i in range(len(temp_list)):
+    label_dict[i] = temp_list[i]
+    int_labels.append(i)
+
+def cambio_str2int(a):
+    val = a["label"]
+    for k in int_labels:
+        if(val == temp_list[k]):
+            #Trovato uguale, il suo numero è k
+            #print("PRIMA#",k ,": ", a["label"])
+            a["label"] = k
+            #print("DOPO#",k ,": ", a["label"])
+
+    return a
+            
+print(dataset["train"][predictions.item()]["label"])
